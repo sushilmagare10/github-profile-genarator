9b6baf75f8d0947d04b351c658fc35f086f9a768
Sushil Magare <sushilmagare10@gmail.com>
2024-07-29 21:58:48 +0530

    Updated the sidebar

--- components/ProfileForm/ProfileForm.tsx
+++ components/ProfileForm/ProfileForm.tsx
@@ -1,15 +1,31 @@
+"use client"
+
 import React from 'react'
 import { Card, CardContent, CardHeader, CardTitle } from '../ui/card'
 import { Input } from '../ui/input'
 import { Textarea } from '../ui/textarea'
 import Introduction from './Introduction'
 import Skills from './Skills'
+import useSidebarStore from '@/store/SidebarStore'
 
 const ProfileForm = () => {
+    const { activeSection, setActiveSection } = useSidebarStore();
+
+    const renderActionSection = () => {
+        switch (activeSection) {
+            case "skills":
+                return <Skills />;
+
+            case "introduction":
+            default:
+                return <Introduction />
+        }
+    }
+
     return (
         <div className=' sticky top-4 flex flex-col w-full justify-start items-center gap-4 bg-card p-4 h-full border border-black/25 shadow-lg rounded-md'>
-            <Introduction />
-            <Skills />
+
+            {renderActionSection()}
         </div>
     )
 }
--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,25 +1,36 @@
+"use client"
+
 import React from 'react'
 import { Button } from '../ui/button'
+import useSidebarStore from '@/store/SidebarStore';
 
 const Sidebar = () => {
 
+    const { activeSection, setActiveSection } = useSidebarStore();
+
+    const sections = [
+        { id: 'introduction', label: 'Introduction' },
+        { id: 'social', label: 'Social' },
+        { id: 'skills', label: 'Skills' },
+        { id: 'bagdes', label: 'Bagdes' },
+    ]
 
     return (
         <section className='sticky top-4 flex flex-col mt-4 items-center justify-start p-2 rounded-md gap-4 min-w-80 bg-slate-100 h-full shadow-xl border border-black/25 '>
             <h2 className='text-3xl text-primary uppercase font-bold mt-4 ml-6 w-full'>Profilify</h2>
             <div className=' flex flex-col mt-8 gap-4 w-full p-4 h-full rounded-md'>
-                <Button className='py-7 font-semibold'>
-                    Introduction
-                </Button>
-                <Button className='py-7 font-semibold'>
-                    Skills
-                </Button>
-                <Button className='py-7 font-semibold'>
-                    Badges
-                </Button>
-                <Button className='py-7 font-semibold'>
-                    Support
-                </Button>
+                {
+                    sections.map((section) => (
+                        <Button
+                            key={section.id}
+                            variant={activeSection === section.id ? 'default' : 'outline'}
+                            className="w-full justify-start"
+                            onClick={() => setActiveSection(section.id)}
+                        >
+                            {section.label}
+                        </Button>
+                    ))
+                }
             </div>
         </section>
     )
