3cab4669cf02cea0626a59775b0420e88ebbeb51
Sushil Magare <sushilmagare10@gmail.com>
2024-07-29 22:00:54 +0530

    added skills and opton to select skill

--- components/ProfileForm/Skills.tsx
+++ components/ProfileForm/Skills.tsx
@@ -1,19 +1,47 @@
 "use client"
 
-import Image from 'next/image'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
+import { Card, CardContent, CardHeader, CardTitle } from '../ui/card'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../ui/select'
+import { Checkbox } from '../ui/checkbox'
+import { SkillsData } from '@/data/SkillsData'
 
 const Skills = () => {
+    const [selectedStyle, setSelectedStyle] = useState('flat')
 
+    const handleStyleChange = (value) => {
+        setSelectedStyle(value)
+    }
 
     return (
-        <div className=' '>Skills
-            <img
-                src='https://img.shields.io/badge/C%23-239120?style=for-the-badge&logo=c-sharp&logoColor=white'
-                alt='c#'
-                className='w-12 h-12 object-center rounded-full'
-            />
-        </div>
+        <Card className='w-full p-4 flex flex-col gap-4'>
+            <CardTitle>Skills</CardTitle>
+            <CardContent className='p-0'>
+                <Select onValueChange={handleStyleChange}>
+                    <SelectTrigger className="w-[200px]">
+                        <SelectValue placeholder="Select Style" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectItem value="flat">Flat</SelectItem>
+                        <SelectItem value="flat-square">Flat-Square</SelectItem>
+                        <SelectItem value="plastic">Plastic</SelectItem>
+                        <SelectItem value="for-the-badge">For-the-Badge</SelectItem>
+                        <SelectItem value="social">Social</SelectItem>
+                    </SelectContent>
+                </Select>
+            </CardContent>
+            <CardContent className='p-0 mt-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+                {SkillsData.map((skill) => (
+                    <Card key={skill.id} className='flex flex-col justify-center items-center p-2 h-20'>
+                        <img
+                            src={`${skill.url.split('?')[0]}?style=${selectedStyle}&${skill.url.split('?')[1] || ''}`}
+                            alt={skill.label}
+                            className=' min-w-16 min-h-7 rounded-md'
+                        />
+                    </Card>
+                ))}
+            </CardContent>
+        </Card>
     )
 }
 
--- components/ProfilePreview.tsx
+++ components/ProfilePreview.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import { Card, CardContent, CardHeader } from './ui/card'
+import { Card, CardContent, CardHeader, CardTitle } from './ui/card'
 
 const ProfilePreview = () => {
     return (
         <div className='flex flex-col justify-start items-center bg-card p-4 h-full w-full border border-black/25 shadow-lg rounded-md'>
-            <Card className='h-full'>
+            <Card className='h-full w-full p-4'>
+                <CardTitle>
+                    Preview
+                </CardTitle>
                 <CardHeader>
                     Introduction
                 </CardHeader>
