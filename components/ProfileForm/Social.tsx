fcdf065c5997fa7bd9804cc4a67dbc7057ed9f91
Sushil Magare <sushilmagare10@gmail.com>
2024-08-01 20:50:50 +0530

    Added the socialdata and updated the social component

--- components/ProfileForm/Social.tsx
+++ components/ProfileForm/Social.tsx
@@ -4,51 +4,95 @@ import React, { useState } from 'react'
 import { Card, CardContent, CardTitle } from '../ui/card'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../ui/select'
 import { SocialData } from '@/data/SocialData'
+import { Button } from '../ui/button'
+import { Input } from '../ui/input'
 
 const Social = () => {
-
     const [selectedStyle, setSelectedStyle] = useState('flat')
+    const [selectedProvider, setSelectedProvider] = useState("shieldIcons")
+    const [usernames, setUsernames] = useState({});
+
+
     const handleStyleChange = (value) => {
         setSelectedStyle(value)
     }
 
+    const handleProviderChange = (value) => {
+        setSelectedProvider(value)
+    }
+
+    const handleUsernameChange = (id, value) => {
+        setUsernames(prev => ({ ...prev, [id]: value }));
+    };
+
+    const socialData = SocialData[selectedProvider] || []
 
     return (
-        <Card className=' w-full p-4 flex flex-col gap-4'>
+        <Card className=' w-full p-0 flex flex-col border-none gap-4'>
             <CardTitle>Social</CardTitle>
             <CardContent className='p-0'>
-                <Select onValueChange={handleStyleChange} defaultValue='Shields'>
-                    <SelectTrigger className='w-[200px]'>
-                        <SelectValue placeholder='Select Provider' />
-                    </SelectTrigger>
-                    <SelectContent >
-                        <SelectItem value="Shields">Shields</SelectItem>
-                        <SelectItem value="Devicons">Devicons</SelectItem>
-                        <SelectItem value="SimpleIcons">SimpleIcons</SelectItem>
-                        <SelectItem value="SkillsIcons">SkillsIcons</SelectItem>
-                    </SelectContent>
-                </Select>
-                <Select onValueChange={handleStyleChange} >
-                    <SelectTrigger className="w-[200px] mt-4">
-                        <SelectValue placeholder="Select Style" />
-                    </SelectTrigger>
-                    <SelectContent>
-                        <SelectItem value="flat">Flat</SelectItem>
-                        <SelectItem value="flat-square">Flat-Square</SelectItem>
-                        <SelectItem value="plastic">Plastic</SelectItem>
-                        <SelectItem value="for-the-badge">For-the-Badge</SelectItem>
-                        <SelectItem value="social">Social</SelectItem>
-                    </SelectContent>
-                </Select>
-            </CardContent>
+                <div className='flex items-center gap-4'>
+                    <Select onValueChange={handleProviderChange} defaultValue="shieldIcons">
+                        <SelectTrigger className="w-[200px]">
+                            <SelectValue placeholder="Select Provider" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectItem value="shieldIcons">Shields Icons</SelectItem>
+                            <SelectItem value="simpleIcons">Simple Icons</SelectItem>
+                            <SelectItem value="githubRaw">GitHub Raw Icons</SelectItem>
+                        </SelectContent>
+                    </Select>
+                    {selectedProvider === 'shieldIcons' && (
+                        <Select onValueChange={handleStyleChange} defaultValue="flat">
+                            <SelectTrigger className="w-[200px]">
+                                <SelectValue placeholder="Select Style" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectItem value="flat">Flat</SelectItem>
+                                <SelectItem value="flat-square">Flat-Square</SelectItem>
+                                <SelectItem value="plastic">Plastic</SelectItem>
+                                <SelectItem value="for-the-badge">For-the-Badge</SelectItem>
+                            </SelectContent>
+                        </Select>
+                    )}
+                </div>
 
-            <CardContent className='p-0 mt-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
-                {SocialData.map((social) => (
-                    <Card key={social.id} className='flex flex-col justify-center items-center p-2 h-20 cursor-pointer shadow-md'>
+            </CardContent>
+            <CardContent className='flex flex-col w-full gap-2 p-0'>
+                <CardTitle>Edit</CardTitle>
+                <div className='flex  justify-between -mt-1'>
+                    <div className='flex flex-col'>
+                        <h3 className='mt-2 font-semibold'>Section Style</h3>
+                        <div className=' flex items-center gap-4 mt-2'>
+                            <Button className='px-6 h-8' variant='outline'>Left</Button >
+                            <Button className='px-6 h-8' variant='outline'>Center</Button >
+                            <Button className='px-6 h-8' variant='outline'>Right</Button >
+                        </div>
+                    </div>
+                    <div>
+                        <h3 className='mt-2 font-semibold'>Gap</h3>
+                        <div className=' flex items-center gap-4 mt-2'>
+                            <Input placeholder='Gap 2px' className='h-8' />
+                        </div>
+                    </div>
+                </div>
+                <CardTitle className='mt-4'>Selected Icons</CardTitle>
+                <Card className='w-full h-28 '>
+
+                </Card>
+            </CardContent>
+            <CardContent className='p-0 mt-4 grid grid-cols-2 gap-4'>
+                {socialData.map((social) => (
+                    <Card key={social.id} className='flex flex-col justify-center items-center p-2 h-28 cursor-pointer shadow-md'>
                         <img
-                            src={`${social.url.split('?')[0]}?style=${selectedStyle}&${social.url.split('?')[1] || ''}`}
+                            src={selectedProvider === 'shieldIcons' ? `${social.url.split('?')[0]}?style=${selectedStyle}&${social.url.split('?')[1] || ''}` : social.url}
                             alt={social.label}
-                            className='min-w-16 min-h-7 rounded-md'
+                            className='min-w-16 min-h-7 rounded-md self-start '
+                        />
+                        <Input
+                            value={social.href(usernames[social.id] || '')}
+                            onChange={(e) => handleUsernameChange(social.id, e.target.value.split('/').pop())}
+                            className=' border-black/20 self-start mt-2'
                         />
                     </Card>
                 ))}
