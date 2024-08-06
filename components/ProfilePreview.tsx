9f1303d4d25d720d2655da434ea6cdb34de5b975
Sushil Magare <sushilmagare10@gmail.com>
2024-08-06 21:16:13 +0530

    Updated the profilepreview and the sidebar

--- components/ProfilePreview.tsx
+++ components/ProfilePreview.tsx
@@ -1,22 +1,113 @@
-import React from 'react'
-import { Card, CardContent, CardHeader, CardTitle } from './ui/card'
+"use client"
 
-const ProfilePreview = () => {
-    return (
-        <div className='flex flex-col justify-start items-center bg-card p-4 h-full w-full border border-black/25 shadow-lg rounded-md'>
-            <Card className='h-full w-full p-4'>
-                <CardTitle>
-                    Preview
-                </CardTitle>
-                <CardHeader>
-                    Introduction
-                </CardHeader>
-                <CardContent>
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import useIntroductionStore from '@/store/IntroStore';
+import useSocialStore from '@/store/SocialStore';
 
-                </CardContent>
-            </Card>
-        </div>
-    )
+
+const heightValues = {
+  xs: 16,
+  sm: 18,
+  md: 24,
+  lg: 28,
+  xl: 32,
+};
+
+const gapValues = {
+  xs: 4,
+  sm: 8,
+  md: 12,
+  lg: 16,
+  xl: 18
 }
 
-export default ProfilePreview
+const ProfilePreview = () => {
+  const {
+    headerImage,
+    name,
+    aboutMe,
+    currentlyDoing,
+    visibleFields,
+    fieldStyles,
+  } = useIntroductionStore();
+
+  const { gap, icons, sectionStyle, userName, iconHeight } = useSocialStore();
+
+  const applyStyle = (content, style) => {
+    const styledContent = (
+      <div
+        style={{
+          fontSize: `${style.fontSize}px`,
+          textAlign: style.alignment,
+          fontWeight: style.bold ? 'bold' : 'normal',
+          fontStyle: style.italic ? 'italic' : 'normal',
+        }}
+      >
+        {content}
+      </div>
+    );
+    return styledContent;
+  };
+
+
+  return (
+    <div className='flex flex-col justify-start items-center bg-card p-4 h-full max-h-screen border border-black/40 w-full shadow-lg rounded-md'>
+      <div className='h-full w-full border-none'>
+        <h1>Preview</h1>
+        <div className='mt-2 space-y-4'>
+          {visibleFields.headerImage && headerImage && (
+            <div style={{ textAlign: 'center' }}>
+              {headerImage.startsWith('http') ? (
+                <img src={headerImage} alt={name} />
+              ) : (
+                <div>{headerImage}</div>
+              )}
+            </div>
+          )}
+          {visibleFields.introduction && name && applyStyle(` ${name}`, fieldStyles.name)}
+          {visibleFields.aboutMe && aboutMe && applyStyle(aboutMe, fieldStyles.aboutMe)}
+          {(currentlyDoing.learning || currentlyDoing.askMeAbout || currentlyDoing.funFact) && (
+            <div>
+              <h3 className='font-bold my-2'>Rapid Fire:</h3>
+              <ul>
+                {currentlyDoing.learning && (
+                  <li>🌱 I'm currently learning <strong>{currentlyDoing.learning}</strong></li>
+                )}
+                {currentlyDoing.askMeAbout && (
+                  <li>💬 Ask me about <strong>{currentlyDoing.askMeAbout}</strong></li>
+                )}
+                {currentlyDoing.funFact && (
+                  <li>⚡ Fun fact: <strong>{currentlyDoing.funFact}</strong></li>
+                )}
+              </ul>
+            </div>
+          )}
+          <h3 className='font-bold '>Connect with me:</h3>
+          <div
+            style={{
+              display: 'flex',
+              flexWrap: 'wrap',
+              columnGap: `${gapValues[gap]}px`,
+              rowGap: "6px",
+              justifyContent: sectionStyle,
+            }}
+          >
+            {icons.map((icon, index) => (
+              <a key={index} href={icon.href} target="_blank" rel="noopener noreferrer">
+                <img
+                  src={icon.url}
+                  alt={userName}
+                  height={iconHeight}
+                  style={{ height: `${heightValues[iconHeight]}px`, }} />
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfilePreview;
--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -17,8 +17,8 @@ const Sidebar = () => {
     ]
 
     return (
-        <section className='sticky top-4 flex flex-col mt-4 items-center justify-start p-2 rounded-md gap-4 min-w-80 bg-slate-100 h-full shadow-xl border border-black/25 '>
-            <h2 className='text-3xl text-primary uppercase font-bold mt-4 ml-6 w-full'>Profilify</h2>
+        <section className='sticky top-4 flex flex-col mt-4 items-center justify-start p-2 rounded-md gap-4 min-w-80 bg-card h-full shadow-xl border border-black/25 '>
+            <h2 className='text-3xl text-primary uppercase font-bold mt-4 ml-6 w-full'>GRG</h2>
             <div className=' flex flex-col mt-8 gap-4 w-full p-4 h-full rounded-md'>
                 {
                     sections.map((section) => (
