1027f3d4892c1564cff337654f14f57e2f3f0e73
Sushil Magare <sushilmagare10@gmail.com>
2024-08-21 20:29:52 +0530

    Updated the intro

--- components/ProfileForm/Introduction.tsx
+++ components/ProfileForm/Introduction.tsx
@@ -1,16 +1,14 @@
-"use client"
+"use client";
 
-import React, { ReactNode } from 'react'
-import { Card, CardContent, CardHeader } from '../ui/card'
-import { Input } from '../ui/input'
-import { Textarea } from '../ui/textarea'
-import { Checkbox } from '../ui/checkbox'
-import { Label } from '../ui/label'
-import useIntroductionStore from '@/store/IntroStore'
-import { motion } from 'framer-motion'
-import { FaImage, FaUser, FaInfoCircle, FaTasks } from 'react-icons/fa'
-import StyleOptions from './StyleOptions'
-import { title } from 'process'
+import React, { ReactNode } from 'react';
+import { Card, CardContent, CardHeader } from '../ui/card';
+import { Input } from '../ui/input';
+import { Checkbox } from '../ui/checkbox';
+import { Label } from '../ui/label';
+import useIntroductionStore from '@/store/IntroStore';
+import { motion } from 'framer-motion';
+import { FaImage, FaUser, FaInfoCircle, FaTasks } from 'react-icons/fa';
+import StyleOptions from './StyleOptions';
 
 const Introduction = () => {
     const {
@@ -26,53 +24,45 @@ const Introduction = () => {
         setAboutMe,
         setCurrentlyDoing,
         toggleField,
-    } = useIntroductionStore()
+    } = useIntroductionStore();
 
     return (
-        <div className='w-full'>
-            <motion.div
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.5 }}
-                className='flex flex-col justify-center items-center w-full gap-6'
-            >
+        <Card className='w-full p-4 flex flex-col border border-gray-200 rounded-lg shadow-sm'>
+            <div className='space-y-8'>
                 <ToggleableSection
                     title="Add Header Image"
-                    icon={<FaImage />}
+                    icon={<FaImage className="text-blue-600 text-xl" />}
                     isVisible={visibleFields.headerImage}
                     onToggle={() => toggleField('headerImage')}
                 >
-                    <Card className='w-full hover:shadow-lg transition-shadow duration-300'>
-                        <CardHeader>Image URL</CardHeader>
+                    <Card className='w-full border border-gray-300 rounded-md shadow-sm '>
+                        <CardHeader className="text-lg font-semibold ">Image URL</CardHeader>
                         <CardContent>
                             <Input
                                 placeholder='https://example.com/image.jpg'
                                 type='url'
                                 value={headerImage}
                                 onChange={(e) => setHeaderImage(e.target.value)}
+                                className="w-full rounded-md shadow-sm "
                             />
                         </CardContent>
                     </Card>
-                    {/* <StyleOptions
-                        sectionName="Header Image"
-                        style={fieldStyles.headerImage}
-                        onStyleChange={(style) => setFieldStyle('headerImage', style)}
-                    /> */}
                 </ToggleableSection>
 
                 <ToggleableSection
                     title="Add Introduction"
-                    icon={<FaUser />}
+                    icon={<FaUser className="text-green-600 text-xl" />}
                     isVisible={visibleFields.introduction}
                     onToggle={() => toggleField('introduction')}
                 >
-                    <Card className='w-full hover:shadow-lg transition-shadow duration-300'>
-                        <CardHeader>Hi, my name is</CardHeader>
+                    <Card className='w-full  rounded-md shadow-sm '>
+                        <CardHeader className="text-lg font-semibold ">Hi, my name is</CardHeader>
                         <CardContent>
                             <Input
-                                placeholder='John Doe'
                                 value={name}
                                 onChange={(e) => setName(e.target.value)}
+                                placeholder="John Doe"
+                                className="w-full border-gray-300 rounded-md shadow-sm "
                             />
                         </CardContent>
                     </Card>
@@ -85,18 +75,18 @@ const Introduction = () => {
 
                 <ToggleableSection
                     title="Add About Me"
-                    icon={<FaInfoCircle />}
+                    icon={<FaInfoCircle className="text-purple-600 text-xl" />}
                     isVisible={visibleFields.aboutMe}
                     onToggle={() => toggleField('aboutMe')}
                 >
-                    <Card className='w-full hover:shadow-lg transition-shadow duration-300'>
-                        <CardHeader>About me</CardHeader>
+                    <Card className='w-full border border-gray-300 rounded-md shadow-sm '>
+                        <CardHeader className="text-lg font-semibold ">About me</CardHeader>
                         <CardContent>
-                            <Textarea
-                                placeholder='Tell us about yourself'
+                            <Input
                                 value={aboutMe}
                                 onChange={(e) => setAboutMe(e.target.value)}
-                                className='min-h-[100px]'
+                                placeholder="Tell us about yourself"
+                                className="w-full border-gray-300 rounded-md shadow-sm "
                             />
                         </CardContent>
                     </Card>
@@ -109,39 +99,35 @@ const Introduction = () => {
 
                 <ToggleableSection
                     title="Add Currently Doing"
-                    icon={<FaTasks />}
+                    icon={<FaTasks className="text-orange-600 text-xl" />}
                     isVisible={visibleFields.currentlyDoing}
                     onToggle={() => toggleField('currentlyDoing')}
                 >
-                    <Card className='w-full hover:shadow-lg transition-shadow duration-300'>
+                    <Card className='w-full border border-gray-300 h-full rounded-md shadow-sm '>
                         <CardContent className='space-y-4 mt-4'>
                             <Input
-                                placeholder='🌱 I&apos;m currently learning'
                                 value={currentlyDoing.learning}
                                 onChange={(e) => setCurrentlyDoing('learning', e.target.value)}
+                                placeholder="🌱 I'm currently learning"
+                                className="w-full border-gray-300 rounded-md shadow-sm "
                             />
-
                             <Input
-                                placeholder='❓ Ask me about anything related'
                                 value={currentlyDoing.askMeAbout}
                                 onChange={(e) => setCurrentlyDoing('askMeAbout', e.target.value)}
+                                placeholder="❓ Ask me about anything related"
+                                className="w-full border-gray-300 rounded-md shadow-sm "
                             />
-
                             <Input
-                                placeholder='⚡ Fun fact: '
                                 value={currentlyDoing.funFact}
                                 onChange={(e) => setCurrentlyDoing('funFact', e.target.value)}
+                                placeholder="⚡ Fun fact: "
+                                className="w-full border-gray-300 rounded-md shadow-sm "
                             />
                         </CardContent>
                     </Card>
-                    {/* <StyleOptions
-                        sectionName="Currently Doing"
-                        style={fieldStyles.currentlyDoing}
-                        onStyleChange={(style) => setFieldStyle('currentlyDoing', style)}
-                    /> */}
                 </ToggleableSection>
-            </motion.div>
-        </div>
+            </div>
+        </Card>
     )
 }
 
@@ -154,29 +140,24 @@ type ToggleableSectionType = {
 };
 
 const ToggleableSection = ({ title, icon, isVisible, onToggle, children }: ToggleableSectionType) => (
-    <div className='space-y-4 w-full'>
-        <div className='flex items-center space-x-2'>
+    <div className='w-full'>
+        <div className='flex items-center space-x-3 mb-4'>
             <Checkbox
                 checked={isVisible}
                 onCheckedChange={onToggle}
-                className="h-5 w-5"
+                className="h-5 w-5 text-blue-600 rounded-sm"
             />
-            <Label className="text-lg font-semibold flex items-center space-x-2">
+            <Label className="text-xl font-semibold flex items-center space-x-2 ">
                 {icon}
                 <span>{title}</span>
             </Label>
         </div>
         {isVisible && (
-            <motion.div
-                initial={{ opacity: 0, height: 0 }}
-                animate={{ opacity: 1, height: 'auto' }}
-                exit={{ opacity: 0, height: 0 }}
-                transition={{ duration: 0.3 }}
-            >
+            <>
                 {children}
-            </motion.div>
+            </>
         )}
     </div>
 )
 
-export default Introduction
+export default Introduction;
