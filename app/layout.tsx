430ea2370b5344e8ed6c85a997d9ec935c3b4965
Sushil Magare <sushilmagare10@gmail.com>
2024-09-09 21:57:00 +0530

    Added the nextjs analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} w-full h-full bg-background overflow-hidden`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
