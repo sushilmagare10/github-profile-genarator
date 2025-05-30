9d828836e0c7f2e29ef5526e0454d6edf5e8185e
Sushil Magare <sushilmagare10@gmail.com>
2025-05-30 20:06:15 +0530

    add: new theme and dark & light mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} w-full h-full bg-background overflow-hidden`}>
-        {children}
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+          {children}
+          </ThemeProvider>
       </body>
     </html>
   );
